7e193853ef97fd4897c7b0a1c9fa5cf866caf248
mz-scripter-X <161574712+mz-scripter-X@users.noreply.github.com>
2024-03-04 23:14:29 +0100

    Create main.py
    This is the main part of the game, it combines all the other classes together.

---
+++ main.py
@@ -0,0 +1,46 @@
+import time
+from turtle import Screen
+
+from food import Food
+from snake import Snake
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("brown")
+screen.title("Snake Bite!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.refresh()
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+screen.exitonclick()
