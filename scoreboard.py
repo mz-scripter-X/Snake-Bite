0aed106a4d9ea7b7c8d5e2b41398aed901b5e654
mz-scripter-X <161574712+mz-scripter-X@users.noreply.github.com>
2024-03-04 23:19:01 +0100

    Create scoreboard.py
    This displays your score and the highest score

---
+++ scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 265)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+                self.score = 0
+        self.update_score()
+
+    def refresh(self):
+        self.score += 1
+        self.update_score()
